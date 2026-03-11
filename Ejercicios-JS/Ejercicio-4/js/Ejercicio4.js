26890432ded6d9b09dc8c333d2cc21e10c7b20e3
Noelia <noeliamonts1@gmail.com>
2026-03-11 11:46:29 +0100

    todo tipo de cambios nuevos

--- Ejercicios-JS/Ejercicio-4/js/Ejercicio4.js
+++ Ejercicios-JS/Ejercicio-4/js/Ejercicio4.js
@@ -1,29 +1,46 @@
+//variables
+// Definimos los caracteres que vamos a usar
 let caracteres = "abcdefghijklmnopqrstuvwxyz";
- let caracteresMayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
- let caracteresNumeros = "0123456789";
- let caracteresEspeciales = "!@#$%^&*()_+|~`-={}[]:;'<>?,./";
-let longitud = numeroAleatorio(8, 50)
-let password = " "
+let caracteresMayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let caracteresNumeros = "0123456789";
+let caracteresEspeciales = "!@#$%^&*()_+|~`-={}[]:;'<>?,./";
+let password=""
 
- // devuelve un valor aleatorio entre min y max
+//funciones
+// devuelve un valor aleatorio entre min y max
 function numeroAleatorio(min, max) {
- return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let num = caracteres[numeroAleatorio(8, 50)]
-
-let mayus = caracteresMayusculas[numeroAleatorio(1, 8)]
-
-let numero = caracteresNumeros[numeroAleatorio(1, 2)]
-
-let especial= caracteresEspeciales[numeroAleatorio(1, 2)]
-
-for (let i = 0; i < longitud; i++) {
-    let caracter = caracteres + caracteresMayusculas + caracteresNumeros + caracteresEspeciales;
-    password += caracter[numeroAleatorio(0, caracter.length)];
+/*
+    devolver un carácter en cualquiera aleatorio de array 
+*/
+function getCaracteresEspeciales(min,max,array){
+    let veces=numeroAleatorio(min,max)
+    let i=0
+    let posicion=0
+    let caracter=""
+    while (i<veces){
+        posicion=numeroAleatorio(0, array.length-1)
+        caracter+=array[posicion]
+        i++
+    }
+    return caracter
 }
+
+//main
+let cantidad=numeroAleatorio(8, 50)
+let meFaltan=0
+
+password +=getCaracteresEspeciales(1,2,caracteresNumeros)
+password +=getCaracteresEspeciales(1,2,caracteresEspeciales)
+password +=getCaracteresEspeciales(1,1,caracteresMayusculas)
+
+meFaltan=cantidad-password.length
+password +=getCaracteresEspeciales(meFaltan,meFaltan,caracteres)
+
+console.log(password)
 
 //desordenar array
- password = password.split('').sort(() => Math.random() - 0.5).join('');
-
+password = password.split('').sort(() => Math.random() - 0.5).join('');
 console.log(password)
---
+++ examen/js/examen.js
@@ -0,0 +1,14 @@
+let suma = 0;
+
+function sumar(a, b) {
+return a + b;
+}
+for (let suma = 1; suma < suma.length; i++) {
+suma += i;
+}
+console.log("La suma de los números es:", suma [i]);
+
+
+console.log(sumar(3, 5));
+console.log(sumar(10, 20));
+console.log(sumar(7, 8));
